03f821fe5f2ec5811565dff93051a91a1b2e89db
Rafael Pimenta <jrafapimenta@gmail.com>
2022-01-16 21:59:19 +0000

    Full website interface

--- Website/app.js
+++ Website/app.js
@@ -1,4 +1,10 @@
-const btn = document.getElementById("btn");
-btn.addEventListener("click", function(e){
-    alert("aaa");
+const btn_ligar = document.getElementById("btn-ligar");
+const btn_desligar = document.getElementById("btn-desligar");
+
+btn_ligar.addEventListener('click', function(e){
+    alert("ligar")
+})
+
+btn_desligar.addEventListener('click', function(e){
+    alert("desligar")
 })
